29a148fbb91a5f5d36c9da9de40475b621b833b4
Maikon Albuquerque <maikonmorais@gmail.com>
2024-06-17 21:04:11 -0300

    git commit -m "Commit inicial"

---
+++ src/main/java/io/github/maikonalbuquerque/tome_nota/TomeNotaApplication.java
@@ -0,0 +1,13 @@
+package io.github.maikonalbuquerque.tome_nota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomeNotaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomeNotaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/maikonalbuquerque/tome_nota/TomeNotaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.maikonalbuquerque.tome_nota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomeNotaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
