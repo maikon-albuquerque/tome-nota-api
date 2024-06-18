d13ac32df61c51d05fcc43369c51038827e5d59b
Maikon Albuquerque <maikonmorais@gmail.com>
2024-06-17 21:05:09 -0300

    git commit -m "test: insert automatico de registros no banco"

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into note (title, description, created_at, updated_at, is_active) values ( 'Comprar mesa', 'Lembrar de comprar mesa para notebook', now(), now(), true );
+insert into note (title, description, created_at, updated_at, is_active) values ('Lavar roupas', 'Lavar roupa na casa da Flávia', now(), now(), true );
+insert into note (title, description, created_at, updated_at, is_active) values ('Estudar', 'Ler o livro progamando em go', now(), now(), true );
+insert into note (title, description, created_at, updated_at, is_active) values ('Limpar gaveta', 'Retirar coisa da gaveta do trabalho', now(), now(), true );
